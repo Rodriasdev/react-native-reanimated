c62482543de6dabd3b33019b318ebefbe2aef7ef
Rodriasd <veronrodrigo98@gmail.com>
2024-08-16 10:48:49 -0300

    deleted unnecessary code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,5 @@
-import { Button, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Button, StyleSheet, TouchableOpacity, View } from 'react-native';
+import  Constants  from 'expo-constants';
 import Animated,{useSharedValue} from 'react-native-reanimated';
 
 
@@ -16,17 +13,8 @@ export default function HomeScreen() {
 
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+
+      <View style={styles.titleContainer}>
         <TouchableOpacity onPress={handlePress}>
           <Animated.View style={{
             width,
@@ -35,8 +23,8 @@ export default function HomeScreen() {
           }}/>
         </TouchableOpacity>
         <Button onPress={() => width.value = 50} title='reset'/>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+
   );
 }
 
@@ -44,17 +32,6 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'column',
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    marginTop: Constants.statusBarHeight
+  }
 });
